79b4f3f741b6dec7aca52336322bf067b21323fb
Yash Dengre <yash.dengre68@gmail.com>
2020-09-28 22:16:07 +0530

    master : added first Hello world program in Typescript

---
+++ code/Compiled/HelloWorld.js
@@ -0,0 +1,8 @@
+function HelloWorld() {
+    let id = document.getElementById("container");
+    if (id != null) {
+        console.log("Check")
+        id.innerText = "Hello World!";
+    }
+}
+alert("Hello This is a Hellow World Program for TypeScript");
---
+++ code/HelloWorld.ts
@@ -0,0 +1,8 @@
+function HelloWorld()
+{
+    let id = document.getElementById("container");
+    if(id!=null)
+    {id.innerText = "Hello World!";}
+    
+}
+alert("Hello This is a Hellow World Program for TypeScript")
